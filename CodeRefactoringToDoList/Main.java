590d7e99a88674a5e5c67bdb448fd11eca484fe9
Said Hafiz <saidhafiz321@gmail.com>
2023-11-18 14:39:03 +0700

    PushCode

---
+++ CodeRefactoringToDoList/Main.java
@@ -0,0 +1,12 @@
+package CodeRefactoringToDoList;
+
+public class Main {
+
+  public static String[] model = new String[10];
+
+  public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+  public static void main(String[] args) {
+    // viewShowTodoList();
+  }
+}
